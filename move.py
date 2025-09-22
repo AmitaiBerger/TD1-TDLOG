969c35631a554a9353927c0bc2ff58648e9cde5a
Neil-ben <neil.benmouffok@enpc.fr>
2025-09-22 15:20:48 +0200

    Update move.py

--- move.py
+++ move.py
@@ -3,3 +3,12 @@ This module defines the different kinds of moves, for the different kinds of
 players.
 """
 
+def possible_moves(Player):
+  moves=[]
+  if (Player.color!=BLACK): #check if the player is an inspector
+    for c in CONNECTIONS :
+      if c[0]==Player.position:
+        if (c[2] Player.hand  ):
+          moves.append[(c[1],c[2])]
+  else():
+  return moves
